68b8aff21bd290a5aba3f3c41112b0f7c56b42e9
Tatvam <td14@iitbbs.ac.in>
2019-09-06 02:53:27 +0530

    Server Added

--- receiver.js
+++ receiver.js
@@ -1,10 +1,20 @@
 const mqtt = require('mqtt')
+const express = require('express')
+
+const app = express()
+
+
 let client  = mqtt.connect("mqtt://test.mosquitto.org");
 console.log("connected flag  "+client.connected);
 client.on("connect",function(){	
         client.subscribe('Topic07');
         console.log('client has subscribed successfully');
         client.on('message', function (topic, message){
+            app.get('/temp', (req, res) => {
+              res.send(message.toString())
+            })
             console.log(message.toString()); //if toString is not given, the message comes as buffer
           });
 });
+
+app.listen(8000)
--- test.js
+++ test.js
@@ -1,10 +1,25 @@
-const mqtt = require('mqtt')
-let client  = mqtt.connect("mqtt://test.mosquitto.org",{clientId:"mqttjs01"});
-console.log("connected flag  "+client.connected);
-client.on("connect",function(){	
-        client.subscribe('Topic07');
-        console.log('client has subscribed successfully');
-});
-client.on('message', function (topic, message){
-    console.log(message.toString()); //if toString is not given, the message comes as buffer
-  });
+// const Plotly = require('plotly')
+function rand() {
+        return Math.random();
+      }
+      
+Plotly.plot('graph', [{
+y: [1,2,3].map(rand),
+mode: 'lines',
+line: {color: '#80CAF6'}
+}, {
+y: [1,2,3].map(rand),
+mode: 'lines',
+line: {color: '#DF56F1'}
+}]);
+
+var cnt = 0;
+
+var interval = setInterval(function() {
+
+Plotly.extendTraces('graph', {
+        y: [[rand()], [rand()]]
+}, [0, 1])
+
+if(cnt === 100) clearInterval(interval);
+}, 300);
